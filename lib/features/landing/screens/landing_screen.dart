ee352f99db570e424f26e4c70cfe3d04d25af58c
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-04-11 14:42:04 +0000

    auth features done

--- lib/features/landing/screens/landing_screen.dart
+++ lib/features/landing/screens/landing_screen.dart
@@ -16,46 +16,48 @@ class LandingScreen extends StatelessWidget {
     SizeConfig().init(context);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            heightSpace(50),
-            const Text(
-              "Welcome to Whatsapp",
-              style: TextStyle(
-                fontSize: 33,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            mediaHeightSpace(context, 9),
-            Image.asset(
-              "assets/bg.png",
-              height: 340,
-              width: 340,
-              color: tabColor,
-            ),
-            mediaHeightSpace(context, 9),
-            const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Text(
-                'Read our privacy policy. Tap "Agree and continue" to accept the Terms of Service',
-                textAlign: TextAlign.center,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              heightSpace(50),
+              const Text(
+                "Welcome to Whatsapp",
                 style: TextStyle(
-                  color: greyColor,
-                  // fontSize: 14,
-                  // fontWeight: FontWeight.w600,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-            heightSpace(10),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.75,
-              child: CustomButton(
-                text: "AGREE AND CONTINUE",
-                onPressed: () => naviagteToLoginScreen(context),
+              mediaHeightSpace(context, 9),
+              Image.asset(
+                "assets/bg.png",
+                height: 340,
+                width: 340,
+                color: tabColor,
               ),
-            )
-          ],
+              mediaHeightSpace(context, 9),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  'Read our privacy policy. Tap "Agree and continue" to accept the Terms of Service',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: greyColor,
+                    // fontSize: 14,
+                    // fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              heightSpace(10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: CustomButton(
+                  text: "AGREE AND CONTINUE",
+                  onPressed: () => naviagteToLoginScreen(context),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/common/widgets/loader.dart';
 import 'package:whatsapp_clone/features/auth/controller/auth_controller.dart';
-import 'package:whatsapp_clone/features/auth/repository/auth_repository.dart';
 import 'package:whatsapp_clone/features/landing/screens/landing_screen.dart';
 import 'package:whatsapp_clone/firebase_options.dart';
 import 'package:whatsapp_clone/models/user_model.dart';
